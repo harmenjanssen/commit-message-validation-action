630b99a750eb77a39cdc3c5cc0c0b181c20f5d63
Harmen Janssen <harmen@grrr.nl>
2018-12-02 12:40:06 +0100

    Update commit status

--- index.js
+++ index.js
@@ -3,48 +3,61 @@ const createOctokit = require('./lib/github.js');
 
 const SUBJECT_MAXLENGTH = 72;
 
-async function validateMessage(owner, repo, commit_sha) {
+async function updateCommitStatus(octokit, owner, repo, commit_sha, options) {
+  const { state, target_url, description, context } = options;
+  const result = await octokit.repos.createStatus({owner, repo, sha, state, target_url, description, context})
+}
+
+async function validateMessage(octokit, owner, repo, commit_sha) {
+  const commit = await octokit.git.getCommit({ owner, repo, commit_sha });
+  const [ subject ] = commit.data.message.split("\n");
+
+  // Start off simple: check subject length
+  if (subject.length > SUBJECT_MAXLENGTH) {
+    throw new Error(`Subject exceeds maximum length of ${SUBJECT_MAXLENGTH} characters`);
+  }
+
+  // Check artifacts of git commit --fixup or --squash
+  const rebaseInstruction = /^(\!(?:fixup|squash))/.exec(subject);
+  if (rebaseInstruction !== null) {
+    throw new Error(`Commit includes rebase instruction: ${rebaseInstruction[0]}`);
+  }
+
+  // Check whether the commit message starts with an imperative verb
+  const [ firstWord, restWords ] = subject.split(' ');
+  if (!/^[A-Z]{1}/.test(firstWord)) {
+    throw new Error('Subject does not start with an uppercase letter');
+  }
+
+  const wordpos = new WordPOS();
+  if (!wordpos.isVerb(firstWord)) {
+    throw new Error('Subject does not seem to start with a verb');
+  }
+
+  if (/[bcdfghjklmnpqrstvwxz]+ed$/.test(firstWord)) {
+    throw new Error('Subject does not seem to start with an imperative verb');
+  }
+
+}
+
+async function main() {
   const octokit = createOctokit();
+  const [ owner, repo ] = process.env.GITHUB_REPOSITORY.split('/');
   try {
-    const commit = await octokit.git.getCommit({ owner, repo, commit_sha });
-    const [ subject ] = commit.data.message.split("\n");
-
-    // Start off simple: check subject length
-    if (subject.length > SUBJECT_MAXLENGTH) {
-      console.error(`Subject exceeds maximum length of ${SUBJECT_MAXLENGTH} characters`);
-      process.exit(1);
-    }
-
-    // Check artifacts of git commit --fixup or --squash
-    const rebaseInstruction = /^(\!(?:fixup|squash))/.exec(subject);
-    if (rebaseInstruction !== null) {
-      console.error(`Commit includes rebase instruction: ${rebaseInstruction[0]}`);
-      process.exit(1);
-    }
-
-    // Check whether the commit message starts with an imperative verb
-    const [ firstWord, restWords ] = subject.split(' ');
-    if (!/^[A-Z]{1}/.test(firstWord)) {
-      console.error('Subject does not start with an uppercase letter');
-      process.exit(1);
-    }
-
-    const wordpos = new WordPOS();
-    if (!wordpos.isVerb(firstWord)) {
-      console.error('Subject does not seem to start with a verb');
-      process.exit(1);
-    }
-
-    if (/[bcdfghjklmnpqrstvwxz]+ed$/.test(firstWord)) {
-      console.error('Subject does not seem to start with an imperative verb');
-      process.exit(1);
-    }
-
+    await validateMessage(octokit, owner, repo, process.env.GITHUB_SHA);
+    process.exit(0);
+    await updateCommitStatus(octokit, owner, repo, process.env.GITHUB_SHA, {
+      state: 'success',
+      context: 'harmenjanssen/commit-message-validation-action'
+    });
   } catch (err) {
     console.error(err);
+    await updateCommitStatus(octokit, owner, repo, process.env.GITHUB_SHA, {
+      state: 'failure',
+      context: 'harmenjanssen/commit-message-validation-action',
+      description: err
+    });
+
     process.exit(1);
   }
 }
-
-const [ owner, repo ] = process.env.GITHUB_REPOSITORY.split('/');
-validateMessage(owner, repo, process.env.GITHUB_SHA);
