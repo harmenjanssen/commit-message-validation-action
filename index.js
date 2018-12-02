6f540f1b341f986c49a4270ece0e77578caec4d8
Harmen Janssen <harmen@grrr.nl>
2018-12-02 11:09:36 +0100

    Build the checks

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const createOctokit = require('./lib/github.js');
 
+const SUBJECT_MAXLENGTH = 72;
+
 async function validateMessage(owner, repo, commit_sha) {
   const octokit = createOctokit();
   try {
     const commit = await octokit.git.getCommit({ owner, repo, commit_sha });
-    console.log(commit);
+    const [ subject ] = commit.data.message.split("\n");
+
+    // Start off simple: check subject length
+    if (subject.length > SUBJECT_MAXLENGTH) {
+      console.error(`Subject exceeds maximum length of ${SUBJECT_MAXLENGTH} characters`);
+      process.exit(1);
+    }
+
+    // Check artifacts of git commit --fixup or --squash
+    const rebaseInstruction = /^(\!(?:fixup|squash))/.exec(subject);
+    if (rebaseInstruction !== null) {
+      console.error(`Commit includes rebase instruction: ${rebaseInstruction[0]}`);
+      process.exit(1);
+    }
+
+    // Check whether the commit message starts with an imperative verb
+
   } catch (err) {
     console.error(err);
     process.exit(1);
