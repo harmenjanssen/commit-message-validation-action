26efd2a8d0cd4af499a91a69acb00f84d2c3ab24
Harmen Janssen <harmen@grrr.nl>
2018-12-02 10:31:14 +0100

    Noodlin'

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 console.log('hello world');
 console.dir(process.env);
+
+const { exec } = require('child_process');
+
+exec(`git log --format=%B -n 1 ${process.env.GITHUB_SHA}`, (err, stdout) => {
+  console.log(
+    stdout
+  );
+
+
+});
