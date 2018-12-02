fd9a5689dabdf0d9da80dee5d1e82c23a5e6beae
Harmen Janssen <harmen@grrr.nl>
2018-12-02 10:33:09 +0100

    Noodlllliinn

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 console.log('hello world');
 console.dir(process.env);
 
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
 
-exec(`git log --format=%B -n 1 ${process.env.GITHUB_SHA}`, (err, stdout) => {
-  console.log(
-    stdout
-  );
+const stdout = execSync(`git log --format=%B -n 1 ${process.env.GITHUB_SHA}`);
+console.log(
+  stdout
+);
 
-
-});
