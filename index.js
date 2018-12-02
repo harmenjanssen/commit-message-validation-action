11455f96569a45bacad255432193d8f8bf657379
Harmen Janssen <harmen@grrr.nl>
2018-12-02 10:52:41 +0100

    Add error handling

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const createOctokit = require('./lib/github.js');
 
 async function validateMessage(owner, repo, commit_sha) {
-  const github = createOctokit();
-  const commit = await octokit.git.getCommit({ owner , repo, commit_sha });
-  console.log(commit);
+  const octokit = createOctokit();
+  try {
+    const commit = await octokit.git.getCommit({ owner , repo, commit_sha });
+    console.log(commit);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 }
 
 const { owner, repo } = process.env.GITHUB_REPOSITORY.split('/');
