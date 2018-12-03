7d67529b73919370f433cffa11d1cc3e28580655
Harmen Janssen <harmen@grrr.nl>
2018-12-03 21:37:39 +0100

    Add tests and fix isVerb reference

--- index.js
+++ index.js
@@ -1,59 +1,12 @@
-const WordPOS = require('wordpos');
 const createOctokit = require('./lib/github.js');
-
-const SUBJECT_MAXLENGTH = 72;
-
-// Arbitrary list of verbs not in the wordpos list.
-const VERB_EXCEPTIONS = [
-  'refactor',
-  // Although "fixed" is not a valid imperative, it's also not recognized by wordpos,
-  // resulting in a misleading error message.
-  'fixed'
-];
-
-async function isVerb(word) {
-  const wordpos = new WordPOS();
-  const isVerb = await wordpos.isVerb(word);
-  return isVerb || VERB_EXCEPTIONS.includes(word);
-}
-
-async function validateMessage(octokit, owner, repo, commit_sha) {
-  const commit = await octokit.git.getCommit({ owner, repo, commit_sha });
-  const [ subject ] = commit.data.message.split("\n");
-
-  // Start off simple: check subject length
-  if (subject.length > SUBJECT_MAXLENGTH) {
-    throw new Error(`Subject exceeds maximum length of ${SUBJECT_MAXLENGTH} characters`);
-  }
-
-  // Check artifacts of git commit --fixup or --squash
-  const rebaseInstruction = /^(\!(?:fixup|squash))/.exec(subject);
-  if (rebaseInstruction !== null) {
-    throw new Error(`Commit includes rebase instruction: ${rebaseInstruction[0]}`);
-  }
-
-  // Check whether the commit message starts with an imperative verb
-  const [ firstWord, restWords ] = subject.split(' ');
-  if (!/^[A-Z]{1}/.test(firstWord)) {
-    throw new Error('Subject does not start with an uppercase letter');
-  }
-
-  const isVerb = await isVerb(firstWord);
-  if (!isVerb) {
-    throw new Error('Subject does not seem to start with a verb');
-  }
-
-  if (/[bcdfghjklmnpqrstvwxz]+ed$/.test(firstWord)) {
-    throw new Error('Subject does not seem to start with an imperative verb');
-  }
-
-}
+const validateMessage = require('./lib/validate-message.js');
 
 async function main() {
   const octokit = createOctokit();
   const [ owner, repo ] = process.env.GITHUB_REPOSITORY.split('/');
+  const commit = await octokit.git.getCommit({ owner, repo, commit_sha: process.env.GITHUB_SHA });
   try {
-    await validateMessage(octokit, owner, repo, process.env.GITHUB_SHA);
+    await validateMessage(commit.data.message);
     console.log('This commit looks beautiful');
     process.exit(0);
   } catch (err) {
---
+++ test/lib/validate-message.test.js
@@ -0,0 +1,47 @@
+const validateMessage = require('../../lib/validate-message.js');
+
+describe('validateMessage', () => {
+  test('It allows valid messages', async () => {
+    const validMessages = [
+      'Fix the bug',
+      'Refactor the feature',
+      'Update documentation',
+      'Document the feature',
+      'Close the gap',
+      'Bridge the gap',
+      'Redact the stuff',
+      'Add the thing',
+      'Remove the codes',
+      'Count the sheep',
+      'Fire the laser!',
+      'Build the machine'
+    ];
+    expect.assertions(validMessages.length);
+    for (let validMsg of validMessages) {
+      const success = await validateMessage(validMsg);
+      expect(success).toEqual(true);
+    }
+  });
+
+  test('It throws on invalid messages', async () => {
+    const invalidMessages = [
+      ['fix the bug', 'Subject does not start with an uppercase letter'],
+      ['refactor the feature', 'Subject does not start with an uppercase letter'],
+      ['It seems like this is a very long message. Far too long if you ask me. We should do something about that', 'Subject exceeds maximum length of 72 characters'],
+      ['Verb? What verb?', 'Subject does not seem to start with a verb'],
+      ['The bug is gone', 'Subject does not seem to start with a verb'],
+      ['!fixup This commit was made with the --fixup flag', 'Commit includes rebase instruction: !fixup'],
+      ['!squash This commit was made with the --squash flag', 'Commit includes rebase instruction: !squash'],
+      ['Fixed the bug', 'Subject does not seem to start with an imperative verb'],
+      ['Closed the ticket', 'Subject does not seem to start with an imperative verb'],
+    ];
+    expect.assertions(invalidMessages.length);
+    for (let [message, error] of invalidMessages) {
+      try {
+        const success = await validateMessage(message);
+      } catch (err) {
+        expect(err.message).toEqual(error);
+      }
+    }
+  });
+});
