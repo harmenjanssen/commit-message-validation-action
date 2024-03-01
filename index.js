9c57c14033b10cd8b8d24e90d7415d3b741c7bf9
Harmen Janssen <harmen@grrr.nl>
2024-03-01 09:10:32 +0100

    Add a comment with the error to the Pull Request

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
-const createOctokit = require('./lib/github.js');
-const validateMessage = require('./lib/validate-message.js');
+const createOctokit = require("./lib/github.js");
+const validateMessage = require("./lib/validate-message.js");
 
 async function main() {
   const octokit = createOctokit();
-  const [ owner, repo ] = process.env.GITHUB_REPOSITORY.split('/');
-  const commit = await octokit.git.getCommit({ owner, repo, commit_sha: process.env.GITHUB_SHA });
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+  const commit = await octokit.git.getCommit({
+    owner,
+    repo,
+    commit_sha: process.env.GITHUB_SHA,
+  });
   try {
     await validateMessage(commit.data.message);
-    console.log('\n\nThis commit looks beautiful\n\n');
+    console.log("\n\nThis commit looks beautiful\n\n");
     process.exit(0);
   } catch (err) {
+    // Extract the SHA of the commit
+    const commitSha = commit.data.sha;
+    const [commitSubject] = commit.data.message.split("\n");
+
+    // Get the pull requests that contain the commit
+    const { data: pullRequests } = await octokit.pulls.list({
+      owner,
+      repo,
+      sha: commitSha,
+    });
+
+    // Loop through each pull request and add a comment.
+    for (const pullRequest of pullRequests) {
+      if (pullRequest.state !== "open") {
+        continue;
+      }
+      const pullRequestNumber = pullRequest.number;
+
+      await octokit.issues.createComment({
+        owner,
+        repo,
+        issue_number: pullRequestNumber,
+        body: `Oh no! It looks like the commit message of ${commitSha} is invalid.\n
+          Please update it to match the conventional commit format.\n\n
+
+          The message was: \n
+          > ${commitSubject}.\n\n
+
+          Here is the error: ${err.message}`,
+      });
+    }
+
     console.log(`\n\n${err.message}\n\n`);
     process.exit(1);
   }
